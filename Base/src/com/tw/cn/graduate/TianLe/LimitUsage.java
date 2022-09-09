aff6fd2bde8e9820b46b7aa410496ab5172540d4
bakazhou <706515174@qq.com>
2022-09-09 15:30:23 +0800

    feat: add limitListBy2

---
+++ Base/src/com/tw/cn/graduate/TianLe/LimitUsage.java
@@ -0,0 +1,16 @@
+package com.tw.cn.graduate.TianLe;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LimitUsage {
+    public static List<Integer> limitListBy2(List<Integer> integers ){
+        return integers.stream().limit(2).toList();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(4,3,5,1,2,7,6,8,9);
+        List<Integer> limitListBy2 = limitListBy2(integers);
+        System.out.println("list limit 2:"+limitListBy2);
+    }
+}
