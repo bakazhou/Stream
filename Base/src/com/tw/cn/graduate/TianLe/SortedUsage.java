7ffbca5aebcfb0f93c683117c78a96227308ce8e
bakazhou <706515174@qq.com>
2022-09-09 15:27:34 +0800

    feat: add sortedListByAsc

--- Base/src/com/tw/cn/graduate/TianLe/SortedUsage.java
+++ Base/src/com/tw/cn/graduate/TianLe/SortedUsage.java
@@ -4,8 +4,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SortedUsage {
+    //默认升序排列
     public static List<Integer> sortedListByDesc(List<Integer> integers ){
         return integers.stream().sorted().toList();
+    }
+
+    //默认降序排列
+    public static List<Integer> sortedListByAsc(List<Integer> integers ){
+        return integers.stream().sorted(((o1, o2) -> (o1 < o2) ? 1 : -1)).toList();
     }
 
     public static void main(String[] args) {
@@ -13,6 +19,7 @@ public class SortedUsage {
         List<Integer> sortedListByDesc = sortedListByDesc(integers);
         System.out.println("sort list by desc:"+sortedListByDesc);
 
-
+        List<Integer> sortedListByAsc = sortedListByAsc(integers);
+        System.out.println("sort list by asc:"+sortedListByAsc);
     }
 }
