8ea03c79bf0918a5bdd1225b31742691a629f5bf
bakazhou <706515174@qq.com>
2022-09-09 15:40:14 +0800

    feat: add mapTaskId

--- Base/src/com/tw/cn/graduate/TianLe/MapUsage.java
+++ Base/src/com/tw/cn/graduate/TianLe/MapUsage.java
@@ -7,11 +7,17 @@ public class MapUsage {
         return tasks.stream().map(Task::getName).toList();
     }
 
+    private static List<Integer> mapTaskId(List<Task> tasks){
+        return tasks.stream().map(Task::getId).toList();
+    }
+
     public static void main(String[] args) {
         List<Task> tasks = List.of(new Task("task0", 0), new Task("task1", 1));
 
         List<String> taskName = mapTaskName(tasks);
         System.out.println("task name:"+taskName);
 
+        List<Integer> taskId = mapTaskId(tasks);
+        System.out.println(taskId);
     }
 }
--- Base/src/com/tw/cn/graduate/TianLe/Task.java
+++ Base/src/com/tw/cn/graduate/TianLe/Task.java
@@ -13,15 +13,7 @@ public class Task {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 }
