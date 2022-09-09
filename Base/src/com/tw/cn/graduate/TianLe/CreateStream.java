0cd103c6fb5e553b03f6ed9f983518197383b1ec
bakazhou <706515174@qq.com>
2022-09-09 14:12:10 +0800

    feat: add createStreamByList

--- Base/src/com/tw/cn/graduate/TianLe/CreateStream.java
+++ Base/src/com/tw/cn/graduate/TianLe/CreateStream.java
@@ -1,5 +1,7 @@
 package com.tw.cn.graduate.TianLe;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class CreateStream {
@@ -7,5 +9,11 @@ public class CreateStream {
     private static void createStreamByArray(){
         String[] array = {"a","b","c"};
         Stream<String> stream = Stream.of(array);
+    }
+
+    //List集合转Stream
+    private static void createStreamByList(){
+        List<String> list = Arrays.asList("a", "b", "c");
+        Stream<String> stream = list.stream();
     }
 }
