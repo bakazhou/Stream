39a536236a971a268af310d24abc93284905fb9b
bakazhou <706515174@qq.com>
2022-09-13 09:55:20 +0800

    feat: add FindFirstUsage

--- Base/src/com/tw/cn/graduate/TianLe/TerminateOperation/Usage/FindFirstUsage.java
+++ Base/src/com/tw/cn/graduate/TianLe/TerminateOperation/Usage/FindFirstUsage.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-//findAny是找出流中的符合谓词的任意元素
-public class FindAnyUsage {
-    public static Optional<Integer> findAnyByListFieldEqualOne(List<Integer> integers){
+//找到符合谓词的元素流中的第一个元素
+public class FindFirstUsage {
+    public static Optional<Integer> findFirstByListFieldEqualOne(List<Integer> integers){
         return integers.stream().filter(integer -> integer.equals(1)).findAny();
     }
 
     public static void main(String[] args) {
         //set up
         List<Integer> integers = Arrays.asList(1, 1, 1, 1, 1, 0);
-        Optional<Integer> field = findAnyByListFieldEqualOne(integers);
-        System.out.println(field.isPresent());
+
+        Optional<Integer> firstFieldEqualOne = findFirstByListFieldEqualOne(integers);
+
+        System.out.println(firstFieldEqualOne.isPresent());
+
     }
 }
